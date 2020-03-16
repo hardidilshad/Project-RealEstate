cd0ba6e0e130b72f6e7542d0bc85a878ad5f79a9
Hardi Dilshad <hardi.dilshad@hotmail.com>
2020-03-16 22:03:37 +0300

    111

--- H_EstateAgency2/Controllers/HomeController.cs
+++ H_EstateAgency2/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using H_EstateAgency2.Models;
 
 namespace H_EstateAgency2.Controllers
 {  
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
