530864611d7f988d78a79baf92b66084bd4d2cdb
Hardi Dilshad <hardi.dilshad@hotmail.com>
2020-03-16 22:28:06 +0300

    f

--- H_EstateAgency2/Controllers/HomeController.cs
+++ H_EstateAgency2/Controllers/HomeController.cs
@@ -8,23 +8,35 @@ using Microsoft.Extensions.Logging;
 using H_EstateAgency2.Models;
 
 namespace H_EstateAgency2.Controllers
-{  
+{
 
 
 
     //test
     public class HomeController : Controller
     {
+        private readonly AppDBContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
 
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Properties.Where(x => x.isAvailable == true).ToList();
+
+            foreach (var item in result)
+            {
+                var img = _context.Pictures.Where(x => x.PropertyId == item.PropertyId).FirstOrDefault();
+                if (item.pictures == null) item.pictures = new List<Picture>();
+                item.pictures.Add(img);
+            }
+
+            return View(result);
         }
 
         public IActionResult Privacy()
